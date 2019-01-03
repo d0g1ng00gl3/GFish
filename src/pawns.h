53a2f82e135bc01fc947f973c0057bd8ac4239da
d0g1ng00gl3 <d0g1ng00gl3@gmail.com>
2019-01-03 18:38:41 +0300

    Update pawns.h

--- src/pawns.h
+++ src/pawns.h
@@ -39,7 +39,7 @@ struct Entry {
   Bitboard pawn_attacks_span(Color c) const { return pawnAttacksSpan[c]; }
   int weak_unopposed(Color c) const { return weakUnopposed[c]; }
   int pawn_asymmetry() const { return asymmetry; }
-
+  int split_passed_pawns(Color c) const { return splitPassedPawns[c]; }
   int semiopen_file(Color c, File f) const {
     return semiopenFiles[c] & (1 << f);
   }
@@ -72,6 +72,7 @@ struct Entry {
   int semiopenFiles[COLOR_NB];
   int pawnsOnSquares[COLOR_NB][COLOR_NB]; // [color][light/dark squares]
   int asymmetry;
+  int splitPassedPawns[COLOR_NB];
 };
 
 typedef HashTable<Entry, 16384> Table;
